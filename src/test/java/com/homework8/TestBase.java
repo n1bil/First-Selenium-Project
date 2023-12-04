38dafdd0b46490de731a7432d418e02239eaa694
n1bil <nabil.nadzor@gmail.com>
2023-12-04 20:14:37 +0100

    homework 8

---
+++ src/test/java/com/homework8/TestBase.java
@@ -0,0 +1,28 @@
+package com.homework8;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class TestBase {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com/register");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
